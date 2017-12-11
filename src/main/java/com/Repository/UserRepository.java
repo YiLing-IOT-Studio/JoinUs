0b1e6611c77527a7aec91c490b534fb3dcf002dc
zhang_ocean <1125694337@qq.com>
2017-12-11 13:43:51 +0800

    1.1

---
+++ src/main/java/com/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.Repository;
+
+import com.Model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author: zhangocean
+ * @Date: Created in 14:11 2017/12/6
+ * Describe: 数据库操作
+ */
+public interface UserRepository extends JpaRepository<UserInfo, Integer> {
+
+}
